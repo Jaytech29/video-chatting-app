6e319f582e9b4b7a569777f2e09d636707d1a7cd
Deji Hamed <111803982+dhamed007@users.noreply.github.com>
2023-03-30 01:22:00 +0800

    Update main.tf

---
+++ cognito/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    cognito = {
+      source = "joshuarose/cognito"
+      version = "0.1.0"
+    }
+  }
+}
+
+provider "cognito" {
+  # Configuration options
+}
