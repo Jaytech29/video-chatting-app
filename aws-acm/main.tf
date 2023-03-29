384c3695756ff5de19c857699a2826d053497e0a
Deji Hamed <111803982+dhamed007@users.noreply.github.com>
2023-03-30 01:15:21 +0800

    Update main.tf

--- aws-acm/main.tf
+++ aws-acm/main.tf
@@ -1,4 +1,43 @@
+provider "aws" {
+  alias = "acm"
+}
+
+provider "aws" {
+  alias = "route53"
+}
+
 module "acm" {
   source  = "terraform-aws-modules/acm/aws"
-  version = "4.3.2"
+  version = "~> 4.0"
+
+  providers = {
+    aws = aws.acm
+  }
+
+  domain_name  = "my-domain.com"
+
+  subject_alternative_names = [
+    "*.my-domain.com",
+    "app.sub.my-domain.com",
+  ]
+
+  create_route53_records  = false
+  validation_record_fqdns = module.route53_records.validation_route53_record_fqdns
+}
+
+module "route53_records" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> 4.0"
+
+  providers = {
+    aws = aws.route53
+  }
+
+  create_certificate          = false
+  create_route53_records_only = true
+
+  distinct_domain_names = module.acm.distinct_domain_names
+  zone_id               = "Z266PL4W4W6MSG"
+
+  acm_certificate_domain_validation_options = module.acm.acm_certificate_domain_validation_options
 }
